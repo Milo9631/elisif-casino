0bd0bc039fd7e9f6576d882b24b0cf44390c93eb
Milos <143115631+Milo9631@users.noreply.github.com>
2023-12-14 20:58:47 +0000

    Update 4 files

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Elisif
-Formerly known as Sif Casino, Elisif is an all-in-one casino, music, and moderation discord bot built with javascript.
+# shinsetsu
+Formerly known as Sif Casino, shinsetsu is an all-in-one casino, music, and moderation discord bot built with javascript.
 Primarily a casino discord bot, with evolving customizability, tons of casino games, and modernized features rewritten from the old Sif Casino code.
 
 ## Usage
@@ -9,7 +9,7 @@ To run this bot, first clone the source code to your machine. Then, install all 
 npm i
 ```
 
-After dependencies are installed, get your bot credentials. If you have not yet created a bot user, create one [here](https://discord.com/developers/applications). Once you have created a bot user, copy its token. Replace `your token here` with your bot's token in the [.env file](https://github.com/Cannicide/elisif-casino/blob/master/.env). If you require a specific port to run your bot's webserver, you can also modify the port option in the env file; otherwise, the default port value will work.
+After dependencies are installed, get your bot credentials. If you have not yet created a bot user, create one [here](https://discord.com/developers/applications). Once you have created a bot user, copy its token. Replace `your token here` with your bot's token in the [.env file](https://github.com/Cannicide/shinsetsu-casino/blob/master/.env). If you require a specific port to run your bot's webserver, you can also modify the port option in the env file; otherwise, the default port value will work.
 
 After those steps have been finished, start the bot by running the following console command:
 ```
@@ -25,16 +25,16 @@ to another of my bots, Project Sif, and rewrote a bit of Sif Casino to match Sif
 And now, it's 2020. My ZH-bot and Scav-bot discord bots are over twenty times more efficient and neat than even Project Sif's code. So I began the weeks-long quest
 of rewriting the entirety of Sif Casino, every single one of the 21 files, to better match the abilities of my latest bots. And the rewrite was a success!
 
-Elisif is now incredibly modern and efficient. Commands like `/hit` and `/stand` were replaced with message collectors. The help command is no longer a long, ugly list, and instead
+shinsetsu is now incredibly modern and efficient. Commands like `/hit` and `/stand` were replaced with message collectors. The help command is no longer a long, ugly list, and instead
 utilizes an interactive reaction pagination menu. All commands are loaded in via a highly automated, efficient command handler. Certain features that go far beyond
 the bot's regular featureset, such as music, moderation, and logging, will be addable via extension packs that are included with the bot and are toggleable 
-(like modules). And Elisif has never been more customizable. When the Settings Update releases, the bot will go far beyond its current ability to change the command prefix
+(like modules). And shinsetsu has never been more customizable. When the Settings Update releases, the bot will go far beyond its current ability to change the command prefix
 in your guild: extensions will be able to be enabled/disabled, command and music channels will be able to be set, guild starting balance will be customizable, battleship
 turn limits will be modifiable, dollars per message will be customizable with limits, custom commands will be addable, and the bot will have an option to be disabled 
 in your guild if you want to disable it.
 
 ## Features
-Current features of Elisif as of 9/29/2020:
+Current features of shinsetsu as of 9/29/2020:
 - Casino games (battleship, blackjack, coinflip, dice, double, hangman, jackpot, roulette)
 - Paginated help command
 - Bot statistics
@@ -48,7 +48,7 @@ Current features of Elisif as of 9/29/2020:
 - Message interpreter, which interprets non-command messages (currently used to add money on message send)
 - Modernized music (play, stop, pause, resume, skip, queue, and songcontrols commands)
 - Songcontrols command, which uses reaction collectors to produce one-click command-less queue manipulation (looping, previous song, next song)
-- And best of all, Elisif is open-source. All of my code for these features is available to you under the MIT license.
+- And best of all, shinsetsu is open-source. All of my code for these features is available to you under the MIT license.
 
 Additions and fixes as of 4/27/2022:
 - Package.json is now provided in the code
@@ -57,10 +57,10 @@ Additions and fixes as of 4/27/2022:
 - The code now automatically loads environment variables from the .env file
 - README updated with usage and other useful information, as well as notices of current issues
 
-More customizability, especially for settings, was being worked on in upcoming updates, but will no longer be released for Elisif Casino. Instead, those features were implemented in my custom discord.js extension libraries, [node-elisif](https://www.npmjs.com/package/elisif) and [elisif-simple](https://www.npmjs.com/package/elisif-simple), which are both immensely updated and evolved versions of this bot's systems. The node-elisif library is currently being upgraded to discord.js v13, but as of April 2022, the stable version is still running on discord.js v12. I am no longer going to be updating Elisif Casino apart from minor bugfixes, and will instead be working on entirely new Discord bots (powered by the in-progress discord.js v13 version of node-elisif) making use of newer and cooler features.
+More customizability, especially for settings, was being worked on in upcoming updates, but will no longer be released for shinsetsu Casino. Instead, those features were implemented in my custom discord.js extension libraries, [node-shinsetsu](https://www.npmjs.com/package/shinsetsu) and [shinsetsu-simple](https://www.npmjs.com/package/shinsetsu-simple), which are both immensely updated and evolved versions of this bot's systems. The node-shinsetsu library is currently being upgraded to discord.js v13, but as of April 2022, the stable version is still running on discord.js v12. I am no longer going to be updating shinsetsu Casino apart from minor bugfixes, and will instead be working on entirely new Discord bots (powered by the in-progress discord.js v13 version of node-shinsetsu) making use of newer and cooler features.
 
 ## Credits
-**Created by Cannicide#2753**\
+**Created by louane_off**\
 NPM Dependencies: express, discord.js, node-fetch, @discordjs/opus, dotenv (far less dependencies than pre-2.0)\
 API Dependencies: CannicideAPI (abstracts away fetching youtube audio and youtube video details)
 
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 const app = express();
 const fs = require("fs");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Elisif is listening on port ' + listener.address().port);
+const listener = app.listen(process.env.PORT, function () {
+  console.log("shinsetsu is listening on port " + listener.address().port);
 });
 
 //Discord.js initialized
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "/";
 
@@ -28,147 +28,156 @@ const Evg = require("./evg");
 //Guild settings
 const settings = require("./settings");
 
-client.on('guildCreate', guild => {
-    guild.channels.find("name", "general").send("Thanks for adding Elisif to your guild! Use the command /sifcasino to get started.");
-    guild.createRole({
-        name: "Casino",
-        color: "#593695"
-      }).then(role => guild.member(client.user).addRole(role)).catch(console.error);
+client.on("guildCreate", (guild) => {
+  guild.channels
+    .find("name", "general")
+    .send(
+      "Thanks for adding shinsetsu to your guild! Use the command /sifcasino to get started.",
+    );
+  guild
+    .createRole({
+      name: "Casino",
+      color: "#593695",
+    })
+    .then((role) => guild.member(client.user).addRole(role))
+    .catch(console.error);
 });
 
 var commands = [];
 
 /**
-* @type Command[]
-*/
+ * @type Command[]
+ */
 var requisites = [];
 
-client.on('ready', () => {
-    console.log('Elisif is up and running!');
-    client.user.setActivity('/elisifhelp', { type: 'STREAMING', url: 'https://www.twitch.tv/cannicide'});
+client.on("ready", () => {
+  console.log("shinsetsu is up and running!");
+  client.user.setActivity("/shinsetsuhelp", {
+    type: "LISTENING",
+    url: "https://discord.gg/N8YjzTJCYu",
+  });
 
-    //Import commands:
-    var cmdfiles = fs.readdirSync("commands");
-    cmdfiles.forEach((item) => {
-        var file = require(`./commands/${item.substring(0, item.length - 3)}`);
-        if (file instanceof Command) {
-            requisites.push(file);
-        }
-        else if ("commands" in file) {
-            file.commands.forEach((alias) => {
-                if (alias instanceof Command) requisites.push(alias);
-                else if (alias instanceof Alias) requisites.push(alias.getAsCommand());
-            })
-        }
-    });
+  //Import commands:
+  var cmdfiles = fs.readdirSync("commands");
+  cmdfiles.forEach((item) => {
+    var file = require(`./commands/${item.substring(0, item.length - 3)}`);
+    if (file instanceof Command) {
+      requisites.push(file);
+    } else if ("commands" in file) {
+      file.commands.forEach((alias) => {
+        if (alias instanceof Command) requisites.push(alias);
+        else if (alias instanceof Alias) requisites.push(alias.getAsCommand());
+      });
+    }
+  });
 
-    commands = requisites.find(c => c.getName() == "help").getCommands();
+  commands = requisites.find((c) => c.getName() == "help").getCommands();
 });
 
-client.on('message', message => {
-    try {
+client.on("message", (message) => {
+  try {
+    // Avoid bot messages, DM and otherwise:
 
-        // Avoid bot messages, DM and otherwise:
-
-        if (message.author.bot) {
-            return false;
-        }
-
-        //Create new interpreter
-        var intp = new Interpreter(message);
-
-        //Determine prefix
-
-            prefix = settings.get(message.guild.id, "prefix");
-
-        //Command determination:
-
-        var splitter = message.content.replace(" ", ";:splitter185151813367::");
-        var splitted = splitter.split(";:splitter185151813367::");
-
-        var fixRegExp = prefix.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-        var re = new RegExp(fixRegExp);
-
-        var command = splitted[0].replace(re, "");
-        command = command.toLowerCase();
-
-        if (splitted[1]) {
-            var args = splitted[1].split(" ");
-        }
-        else {
-            var args = false;
-        }
-
-        // DM determination:
-
-        if (message.guild === null) {
-            message.reply("Sorry " + message.author.username + ", DM commands are not supported by this bot.");
-
-            return false;
-        }
-
-        //Universal help command:
-
-        if (message.content == "/sifcasino" || message.content == "/elisif" || message.content == "/elisifhelp") {
-            
-            var usable = commands.find(c => c.name == "help").cmd;
-            usable.set(message);
-
-            usable.execute([prefix, args]).catch((err) => {
-                message.reply("An error occurred: " + err);
-            });
-
-            return;
-        }
-        else if (message.content == "/elisifprefix") {
-            //Fetches the prefix (setting the prefix through this is not enabled)
-
-            var usable = commands.find(c => c.name == "prefix").cmd;
-            usable.set(message);
-
-            usable.execute([]).catch((err) => {
-                message.reply("An error occurred: " + err);
-            });
-
-            return;
-        }
-
-        //Check for command:
-        var cmd = false;
-
-        commands.forEach((item, index) => {
-            if (item.name == command) {
-                cmd = item.cmd;
-            }
-        });
-
-        //Run commands if matches prefix:
-
-        if (cmd && splitted[0].match(prefix)) {
-            message.channel.startTyping();
-            setTimeout(() => {
-                cmd.set(message);
-                if (cmd.matches("help")) {
-                    cmd.execute([prefix, args]).catch((err) => {
-                        message.reply("An error occurred: " + err);
-                    });
-                }
-                else {
-                    cmd.execute(args).catch((err) => {
-                        message.reply("An error occurred: " + err);
-                    });
-                }
-                message.channel.stopTyping();
-            }, 1000);
-        }
-        else {
-            intp.interpret(message.content.split(" "));
-        }
-
+    if (message.author.bot) {
+      return false;
     }
-    catch (err) {
-        message.channel.send(`Errors found:\n\`\`\`${err}\nAt ${err.stack}\`\`\``);
+
+    //Create new interpreter
+    var intp = new Interpreter(message);
+
+    //Determine prefix
+
+    prefix = settings.get(message.guild.id, "prefix");
+
+    //Command determination:
+
+    var splitter = message.content.replace(" ", ";:splitter185151813367::");
+    var splitted = splitter.split(";:splitter185151813367::");
+
+    var fixRegExp = prefix.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+    var re = new RegExp(fixRegExp);
+
+    var command = splitted[0].replace(re, "");
+    command = command.toLowerCase();
+
+    if (splitted[1]) {
+      var args = splitted[1].split(" ");
+    } else {
+      var args = false;
     }
+
+    // DM determination:
+
+    if (message.guild === null) {
+      message.reply(
+        "Sorry " +
+          message.author.username +
+          ", DM commands are not supported by this bot.",
+      );
+
+      return false;
+    }
+
+    //Universal help command:
+
+    if (
+      message.content == "/sifcasino" ||
+      message.content == "/shinsetsu" ||
+      message.content == "/shinsetsuhelp"
+    ) {
+      var usable = commands.find((c) => c.name == "help").cmd;
+      usable.set(message);
+
+      usable.execute([prefix, args]).catch((err) => {
+        message.reply("An error occurred: " + err);
+      });
+
+      return;
+    } else if (message.content == "/shinsetsuprefix") {
+      //Fetches the prefix (setting the prefix through this is not enabled)
+
+      var usable = commands.find((c) => c.name == "prefix").cmd;
+      usable.set(message);
+
+      usable.execute([]).catch((err) => {
+        message.reply("An error occurred: " + err);
+      });
+
+      return;
+    }
+
+    //Check for command:
+    var cmd = false;
+
+    commands.forEach((item, index) => {
+      if (item.name == command) {
+        cmd = item.cmd;
+      }
+    });
+
+    //Run commands if matches prefix:
+
+    if (cmd && splitted[0].match(prefix)) {
+      message.channel.startTyping();
+      setTimeout(() => {
+        cmd.set(message);
+        if (cmd.matches("help")) {
+          cmd.execute([prefix, args]).catch((err) => {
+            message.reply("An error occurred: " + err);
+          });
+        } else {
+          cmd.execute(args).catch((err) => {
+            message.reply("An error occurred: " + err);
+          });
+        }
+        message.channel.stopTyping();
+      }, 1000);
+    } else {
+      intp.interpret(message.content.split(" "));
+    }
+  } catch (err) {
+    message.channel.send(`Errors found:\n\`\`\`${err}\nAt ${err.stack}\`\`\``);
+  }
 });
 
 //Added token
